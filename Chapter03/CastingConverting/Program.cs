851c1485dd75f16a911fc9d1fbc5f0c657f90c04
id3og <s4lamandre@outlook.com>
2022-04-17 16:12:16 +0200

    In the middle of chapter 3

---
+++ Chapter03/CastingConverting/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+int a = 10;
+double b = a; // an int can be safely cast into a double
+WriteLine(b);
+
+double c = 9.8;
+// int d = c; // compiler gives an error for this line
+// WriteLine(d);
+
+int d = (int)c;
+WriteLine(d); // d is 9 losing the .8 part
